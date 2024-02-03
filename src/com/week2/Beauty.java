2ff01f3a34ff96c37854a8cf78d3eee779f733da
JinhwanJung-0125 <joung.jinhawn@gmail.com>
2024-02-03 22:36:59 +0900

    Feat : Beauty 클래스 구현

--- src/com/week2/Beauty.java
+++ src/com/week2/Beauty.java
@@ -1,5 +1,26 @@
 package com.week2;
 
-public class Beauty {
+import java.math.BigDecimal;
 
+public class Beauty extends Product implements DeliveryChargeCalculator {
+
+    public Beauty(String name, int price, double weight) throws IllegalArgumentException {
+        super(name, price, weight);
+    }
+
+    @Override
+    public BigDecimal getDeliveryCharge(Double weight, BigDecimal price) {
+        BigDecimal deliveryFee = CalcUtil.getDeliveryFeeByWeight(weight);
+        return CalcUtil.calcDeliveryFeeByPrice(deliveryFee, price);
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer sb = new StringBuffer();
+        sb.append("==========================\n");
+        sb.append("Beauty\n");
+        sb.append(super.toString());
+        sb.append("==========================");
+        return sb.toString();
+    }
 }
--- src/com/week2/LargeAppliance.java
+++ src/com/week2/LargeAppliance.java
@@ -1,5 +1,17 @@
 package com.week2;
 
-public class LargeAppliance {
+public class LargeAppliance extends Product{
+    public LargeAppliance(String name, int price, double weight) throws IllegalArgumentException {
+        super(name, price, weight);
+    }
 
+    @Override
+    public String toString() {
+        StringBuffer sb = new StringBuffer();
+        sb.append("==========================\n");
+        sb.append("Large Appliance\n");
+        sb.append(super.toString());
+        sb.append("==========================");
+        return sb.toString();
+    }
 }
