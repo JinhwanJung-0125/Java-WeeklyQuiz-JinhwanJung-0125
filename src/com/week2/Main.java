d2bbff14ead756f6d47052a5be2936af392ac672
JinhwanJung-0125 <joung.jinhawn@gmail.com>
2024-02-03 23:44:34 +0900

    Feat : main 메소드 작성

--- src/com/week2/Main.java
+++ src/com/week2/Main.java
@@ -2,6 +2,13 @@ package com.week2;
 
 public class Main {
     public static void main(String[] args) {
+        Beauty beauty = new Beauty("Cream", 2500, 1.0);
+        Grocery grocery = new Grocery("Cabbage", 25000, 5.0);
+        LargeAppliance largeAppliance = new LargeAppliance("Refrigerator", 5000000, 450.0);
 
+
+        System.out.println(beauty);
+        System.out.println(grocery);
+        System.out.println(largeAppliance);
     }
 }
