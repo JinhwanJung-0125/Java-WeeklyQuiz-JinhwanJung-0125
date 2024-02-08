c57bb06368de7f70ae8ead49f5fda0befc5c5be3
JinhwanJung-0125 <joung.jinhawn@gmail.com>
2024-02-08 17:30:18 +0900

    Feat : Contact 생성자 구현

--- src/com/week3/Contact.java
+++ src/com/week3/Contact.java
@@ -5,6 +5,15 @@ public class Contact {
     protected String name;
     protected String phoneNumber;
 
+    public Contact(String name, String phoneNumber) {
+        if (checkArgEmpty(name) || checkArgEmpty(phoneNumber)) {
+            throw new IllegalArgumentException("인자로 받은 문자열이 비어있습니다!");
+        }
+
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
     public void setName(String name) {
         if (!checkArgEmpty(name)) {
             this.name = name;
