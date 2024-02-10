557a1665ab524559938828562534108e7de80f93
JinhwanJung-0125 <joung.jinhawn@gmail.com>
2024-02-10 18:21:24 +0900

    Feat : AddressBook 클래스 생성 및 displayContacts(), searchContact() 메소드 추가

--- src/com/week3/AddressBook.java
+++ src/com/week3/AddressBook.java
@@ -1,5 +1,44 @@
 package com.week3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AddressBook {
 
+    private List<Contact> contacts;
+
+    public AddressBook() {
+        this.contacts = new ArrayList<>();
+    }
+
+    public void displayContacts() {
+        if (this.contacts.isEmpty()) {
+            System.out.println("연락처가 비어있습니다.");
+            return;
+        }
+
+        for (Contact contact : this.contacts) {
+            System.out.println(contact);
+        }
+    }
+
+    public void searchContact(String name) {
+        Contact contact = searchContactByName(name);
+
+        if (contact == null) {
+            System.out.println("연락처를 찾을 수 없습니다.");
+            return;
+        }
+        System.out.println(contact);
+    }
+
+    private Contact searchContactByName(String name) {
+        for (Contact contact : this.contacts) {
+            if (contact.getName().equalsIgnoreCase(name)) {
+                return contact;
+            }
+        }
+
+        return null;
+    }
 }
