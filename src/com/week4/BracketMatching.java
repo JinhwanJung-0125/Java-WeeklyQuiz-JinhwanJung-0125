94a6f83b6f2297ab336cc8bb927dc6ea56027c2d
JinhwanJung-0125 <joung.jinhawn@gmail.com>
2024-02-18 18:16:09 +0900

    First Commit

---
+++ src/com/week4/BracketMatching.java
@@ -0,0 +1,15 @@
+package com.week4;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class BracketMatching {
+
+    private static Stack<String> stack = new Stack<>();
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+    }
+}
